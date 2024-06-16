7f14f9d687ecb3c6680417c8a63ac3281fab3f15
renawmontanari <regiangelica@gmail.com>
2024-06-16 15:34:14 -0300

    remover-produto

--- src/components/Rodape/index.jsx
+++ src/components/Rodape/index.jsx
@@ -4,8 +4,8 @@ const Rodape = () => {
   return (
     <footer className="text-center bg-black text-bg-dark">
       <p className="card-text py-3">
-        2023 <i className="bi bi-c-circle"></i> Desenvolvido por Alura | Projeto
-        fictício sem fins comerciais.
+        2024 <i className="bi bi-c-circle"></i> Desenvolvido por Renan W. Montanari | Projeto
+        fictício sem fins comerciais. | T6 ONE - Alura
       </p>
     </footer>
   );
